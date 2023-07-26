2f155d2d3461a92c5719e943ff81c1e652bfa129
AliMostafaRadwan <omar12ali12radwan@gmail.com>
2023-07-26 21:17:47 +0300

    added the roi grabber

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 from streamlit_cropper import st_cropper
 from PIL import Image
 import cv2
+from img_gridder import get_row_image
+import json
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 # Upload an image and set some options for demo purposes
@@ -9,7 +11,7 @@ st.header("Cropper Demo")
 img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'])
 realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
 box_color = st.sidebar.color_picker(label="Box Color", value='#0000FF')
-aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
+# aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
 Number_of_Column_Groups = st.sidebar.number_input(label="Number of Coulmn Groups", min_value=1, max_value=10, value=1, step=1)
 aspect_dict = {
     "1:1": (1, 1),
@@ -18,23 +20,39 @@ aspect_dict = {
     "2:3": (2, 3),
     "Free": None
 }
-aspect_ratio = aspect_dict[aspect_choice]
+aspect_ratio = aspect_dict['Free']
 
 if img_file:
     img = Image.open(img_file)
     if not realtime_update:
         st.write("Double click to save crop")
     # Get a cropped image from the frontend
+    ROI_list = []
     for i in range(Number_of_Column_Groups):
 
-        cropped_img= st_cropper(img, realtime_update=realtime_update, box_color=box_color,
-                                    aspect_ratio=aspect_ratio,return_type='box',key=i)
-        left, top, width, height = cropped_img['left'], cropped_img['top'], cropped_img['width'], cropped_img['height']
-        # print(cropped_img)
-        # st.write(cropped_img)
-        # st.write(left, top, width, height)
-        img = img.crop((left, top, left+width, top+height))
-        st.image(img)
+        cropped_img, rect = st_cropper(img, realtime_update=realtime_update, box_color=box_color,
+                                    aspect_ratio=aspect_ratio,key=i,return_type='image')
+        
+        x, y, width, height = rect['left'], rect['top'], rect['width'], rect['height']
+
+        roi = (x, y, width, height)
+        ROI_list.append(roi)
+        st.write(x, y, width, height)
+        try:
+            num_columns = st.number_input(label="Number of Columns", min_value=1, max_value=100, value=1, step=1, key=x+y)
+            
+            num_rows = st.number_input(label="Number of Rows", min_value=1, max_value=100, value=1, step=1, key=x*y)
+
+            row_number = st.number_input(label="Row Number", min_value=1, max_value=100, value=1, step=1, key=x/y)
+        except:
+            st.write("Please select a region")
+        row_image = get_row_image(img,num_columns,num_rows,row_number,roi)
+        #save the roi for each region as a json file
+        with open('roi.json', 'w') as f:
+            json.dump(ROI_list, f)
+            
+        maxsize = (250,150) 
+        row_image = cv2.resize(row_image,maxsize,interpolation=cv2.INTER_AREA)
+        st.image(row_image)
         # for i in range(Number_of_Column_Groups):
-        # _ = cropped_img.thumbnail((150,150))
         # st.image(cropped_img)
